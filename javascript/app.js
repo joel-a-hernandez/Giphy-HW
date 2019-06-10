6565cefbbc48fa1cf2ae1c3cfae8e3db2423a3d8
Joel Hernandez <joel24hernan@gmail.com>
2019-06-10 18:32:02 -0500

    Added css reset. Moved my js to its own file. added animate and still classes to make gifs start and stop. added small amount of css.

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,59 @@
 var sportsArray = ['basketball', 'soccer', 'football', 'volleyball']
 
-        function displayGifs(){
-            $("#view-gifs").empty();
-            var sport = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=D9PBamVDJrGFTbcyQnYO74HeQQsQDOjm&limit=10";
+    function displayGifs(){
+        $("#view-gifs").empty();
+        var sport = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=D9PBamVDJrGFTbcyQnYO74HeQQsQDOjm&limit=10";
 
-            $.ajax({
+        $.ajax({
             url: queryURL,
             method: "GET",
-            }).then(function(response) {
-                console.log(response.data);
-                var results = response.data;
+        }).then(function(response) {
+            console.log(response.data);
+            var results = response.data;
 
+            
+
+            for (var i = 0 ; i < results.length; i++){
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+            
+                var sportImage = $("<img>");
+                sportImage.attr("src", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-still", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-animate", results[i].images.fixed_height.url);
+                sportImage.attr("data-state", "still");
+                sportImage.attr("class", "gifs");
                 
 
-                for (var i = 0 ; i < results.length; i++){
-                    var gifDiv = $("<div>");
+                gifDiv.prepend(sportImage);
+                gifDiv.prepend(p);
+                
+                var sportGif = $("#view-gifs");
 
-                    var rating = results[i].rating;
-                    var p = $("<p>").text("Rating: " + rating);
+                sportGif.prepend(gifDiv);
 
-                    var sportImage = $("<img>");
-                    sportImage.attr("src", results[i].images.fixed_height.url);
+                $(".gifs").on("click", function() {
+                    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                    var state = $(this).attr("data-state");
 
-                    gifDiv.prepend(sportImage);
-                    gifDiv.prepend(p);
-                    
-                    var sportGif = $("#view-gifs");
+                    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
 
-                    sportGif.prepend(gifDiv);
+                    if (state === "still") {
+                        $(this).attr("data-state", "animate");   // Then, set the image's data-state to animate
+                        $(this).attr("src", $(this).attr("data-animate"));
+                    } else {     // Else set src to the data-still value
+                        $(this).attr("data-state", "still");
+                        $(this).attr("src", $(this).attr("data-still"));
+                    }
+                  });
 
-                   
-                }
-            });
+            }
+        });
 
-        }
+    }
        
    
 
@@ -47,10 +66,10 @@ var sportsArray = ['basketball', 'soccer', 'football', 'volleyball']
             // Looping through the array of movies
             for (var i = 0; i < sportsArray.length; i++) {
 
-            // Then dynamically generating buttons for each movie in the array
+            // Then dynamically generating buttons for each sport in the array
             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var a = $("<button>");
-            // Adding a class of movie to our button
+            // Adding a class of gif to our button
             a.addClass("gif");
             // Adding a data-attribute
             a.attr("data-name", sportsArray[i]);
@@ -78,3 +97,4 @@ var sportsArray = ['basketball', 'soccer', 'football', 'volleyball']
         $(document).on("click", ".gif", displayGifs);
 
         renderButtons();
+        
